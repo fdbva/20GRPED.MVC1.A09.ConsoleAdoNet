2f84044a9c32e80758bea424099cc5d3d60e71f1
Felipe Andrade <villela.felipe@gmail.com>
2020-02-28 07:46:53 -0300

    Add project files.

---
+++ 20GRPED.MVC1.A09.ConsoleAdoNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20GRPED.MVC1.A09.ConsoleAdoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
